5ed7e24b2cd352ebc6f99726e4f55a51efa1b5c4
ROHIT VARSHNEY <varshneyrohit1122@gmail.com>
2022-03-26 09:13:47 +0530

    Create main_test.c

---
+++ 3_Implementation/test/main_test.c
@@ -0,0 +1,28 @@
+#include <salaryandsavingmanage.h>
+#include "unity.h"
+
+
+
+#define PROJECT_NAME    "SALARY AND SAVING MANAGEMENT"
+
+/* Prototypes for all the test functions */
+
+/* Required by the unity test framework */
+void setUp(){}
+/* Required by the unity test framework */
+void tearDown(){}
+
+
+
+int main(void)
+{
+    /* Initiate the Unity Test Framework */
+    UNITY_BEGIN();
+
+    /* Run Test functions */
+    
+
+
+    /* Close the Unity Test Framework */
+    return UNITY_END();
+}
