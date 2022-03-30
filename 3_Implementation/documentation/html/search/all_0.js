4769c827fb9f0ceecce8f04a936f0840e390be3f
ROHIT VARSHNEY <varshneyrohit1122@gmail.com>
2022-03-30 16:41:16 +0530

    update
    update

--- 3_Implementation/documentation/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['documentation_5fmain_0',['documentation_main',['../md_documentation_main.html',1,'']]]
-];
--- 3_Implementation/documentation/html/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['salaryandsavingmanagement_20application_20by_20_22rohit_20varshney_22_0',['SalaryAndSavingManagement Application by &quot;ROHIT VARSHNEY&quot;',['../index.html',1,'']]]
-];
